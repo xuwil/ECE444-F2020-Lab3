010fb7a3fb2dbb37202805b47cfb3bd154d13a39
xuwil <will.xuyudong@gmail.com>
2020-09-30 14:05:42 -0400

    added email address field in web form

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ moment = Moment(app)
 
 class NameForm(Form):
     name = StringField('What is your name?', validators=[DataRequired()])
+    email = StringField('What is your UofT Email Address?', validators=[Email(), DataRequired()])
     submit = SubmitField('Submit')
 
 
@@ -23,11 +24,16 @@ def index():
     form = NameForm()
     if form.validate_on_submit():
         old_name = session.get('name')
+        old_email = session.get('email')
         if old_name is not None and old_name != form.name.data:
             flash('Looks like you have changed your name!')
+        if old_email is not None and old_email != form.email.data:
+            flash('Looks like you have changed your email!')
         session['name'] = form.name.data
+        session['email'] = form.email.data
+
         return redirect(url_for('index'))
-    return render_template('index.html', form=form, name=session.get('name'))
+    return render_template('index.html', form=form, name=session.get('name'), email=session.get('email'))
 
 
 @app.route('/user/<name>')
